1f4263ce7aaaba27b5b20f4f2c7e814e1677245f
felsb3 <felsb3@gmail.com>
2021-07-27 11:46:53 -0300

    Instalado Django Debug Toolbar somente no ambiente de desenvolvimento.
    close #35

--- pypro/urls.py
+++ pypro/urls.py
@@ -1,5 +1,4 @@
 """pypro URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -13,13 +12,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from pypro.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('admin/', admin.site.urls),
     path('', home),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns.append(
+        path('__debug__/', include(debug_toolbar.urls))
+    )
