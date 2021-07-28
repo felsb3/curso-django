ff393efb8a7803ebdd1cc68f69e94f3feece7177
felsb3 <felsb3@gmail.com>
2021-07-28 13:33:44 -0300

    Twitter Bootstrap e Layoutit Instalando Arquivos Estáticos Localmente Template Tag Static Criação de Função para Testar Conteúdo Urls Nomeadas Encapsulando Urls em Apps Favicon, Imagens e HTML Rodapé Entrega da Landing Page close #40

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
 # from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('<html><body>Olá Django!</body></html>')
+    return render(request, 'base/home.html')
