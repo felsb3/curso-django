82a6e89b422857d1524d4ff0e4cf076a6e501e8e
felsb3 <felsb3@gmail.com>
2021-07-29 22:23:11 -0300

    Publicando o Video em Produção close #44

---
+++ pypro/aperitivos/tests.py
@@ -0,0 +1,24 @@
+import pytest
+from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:video', args=('motivacao',)))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_titulo_video(resp):
+    assert_contains(resp, '<h1 class="mt-4 mb-3">Video Aperitivo: Motivação</h1>')
+
+
+def test_conteudo_video(resp):
+    assert_contains(resp, 'iframe width="727" height="409" src="https://www.youtube.com/embed/ipgrkrNCBzA"')
+
+# def test_aperitivo_link(resp):
+#   assert_contains(resp, f'href="{reverse("aperitivo/video.html")}"')
