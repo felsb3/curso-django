82fce5e75068f43edb28ad5fc16d288fa0bea611
felsb3 <felsb3@gmail.com>
2021-07-30 23:44:15 -0300

    Herança de Templates Bloco de Template Contexto de Template close #51

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {'titulo': 'Video Aperitivo: Motivação', 'youtube_id': 'ipgrkrNCBzA'}
+    return render(request, 'aperitivos/video.html', context={'video': video})
+# videos = zLIeu9cPYrY
+# titulo = Como publicar um site Django em MENOS DE 30 MINUTOS — Imersão Django (EP. 1)
+# videos = ipgrkrNCBzA
+# titulo = JORNADA RUMO A PRIMEIRA VAGA!
